f94e9886b9333d9eee6a90b7ae9e727f795c2a73
Naveed Abbas <naveedseoexpert777@gmail.com>
2025-05-05 20:01:15 +0500

    added functionality to result screen

--- ibm_calculator/lib/result_screen.dart
+++ ibm_calculator/lib/result_screen.dart
@@ -1,12 +1,84 @@
 import 'package:flutter/material.dart';
 
 class ResultScreen extends StatelessWidget {
+  final double bmi;
+
+  const ResultScreen({super.key, required this.bmi});
+
+  String getResultText() {
+    if (bmi >= 25) return "Overweight";
+    if (bmi > 18.5) return "Normal";
+    return "Underweight";
+  }
+
+  String getAdvice() {
+    if (bmi >= 25) return "Try to exercise more.";
+    if (bmi > 18.5) return "You have a normal body weight. Good job!";
+    return "You should eat more.";
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Your Result")),
       backgroundColor: Colors.black,
-      body: Text("this is result screen"),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            child: Container(
+              margin: const EdgeInsets.all(15),
+              padding: const EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: const Color(0xFF1D1E33),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    getResultText(),
+                    style: const TextStyle(
+                      color: Colors.green,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Text(
+                    bmi.toStringAsFixed(1),
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 80,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Text(
+                    getAdvice(),
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            color: Colors.red,
+            width: double.infinity,
+            height: 60,
+            child: TextButton(
+              onPressed: () {
+                Navigator.pop(context); // Go back to InputScreen
+              },
+              child: const Text(
+                "RECALCULATE",
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
